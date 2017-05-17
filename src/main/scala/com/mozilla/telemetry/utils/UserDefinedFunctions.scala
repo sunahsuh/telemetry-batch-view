2795b3e20f35d5c1c852c8a56ef40d05e1bf43af
Sunah Suh <ssuh@mozilla.com>
2017-05-17 17:12:31 +0100

    Remove copy-pasted comments on UDFs

--- src/main/scala/com/mozilla/telemetry/utils/UserDefinedFunctions.scala
+++ src/main/scala/com/mozilla/telemetry/utils/UserDefinedFunctions.scala
@@ -16,34 +16,28 @@ class AggregateScalars[T](U: DataType) extends UserDefinedAggregateFunction {
   override def inputSchema: StructType =
     StructType(StructField("value", U) :: Nil)
 
-  // This is the internal fields you keep for computing your aggregate.
   override def bufferSchema: StructType = StructType(
     StructField("aggregate", MapType(U, LongType)) :: Nil
   )
 
-  // This is the output type of your aggregation function.
   override def dataType: DataType = MapType(U, LongType)
 
   override def deterministic: Boolean = true
 
-  // This is the initial value for your buffer schema.
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
     buffer(0) = Map[T, Long]()
   }
 
-  // This is how to update your buffer schema given an input.
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
     val m = buffer.getAs[Map[T, Long]](0)
     val k = input.getAs[T](0)
     buffer(0) = add(m, k)
   }
 
-  // This is how to merge two objects with the bufferSchema type.
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
     buffer1(0) = combine(buffer1.getAs[Map[T, Long]](0), buffer2.getAs[Map[T, Long]](0))
   }
 
-  // This is where you output the final value, given the final value of your bufferSchema.
   override def evaluate(buffer: Row): Any = {
     buffer.getAs[Map[T, Long]](0)
   }
@@ -57,32 +51,26 @@ class AggregateHistograms(histogramLength: Int) extends UserDefinedAggregateFunc
   override def inputSchema: StructType =
     StructType(StructField("value", ArrayType(IntegerType)) :: Nil)
 
-  // This is the internal fields you keep for computing your aggregate.
   override def bufferSchema: StructType = StructType(
     StructField("aggregate", ArrayType(LongType)) :: Nil
   )
 
-  // This is the output type of your aggregation function.
   override def dataType: DataType = ArrayType(LongType)
 
   override def deterministic: Boolean = true
 
-  // This is the initial value for your buffer schema.
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
     buffer(0) = Array.fill[Long](histogramLength)(0L)
   }
 
-  // This is how to update your buffer schema given an input.
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
     buffer(0) = aggregateArrays(buffer.getAs[Seq[Long]](0), input.getAs[Seq[Int]](0).map(_.longValue))
   }
 
-  // This is how to merge two objects with the bufferSchema type.
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
     buffer1(0) = aggregateArrays(buffer1.getAs[Seq[Long]](0), buffer2.getAs[Seq[Long]](0))
   }
 
-  // This is where you output the final value, given the final value of your bufferSchema.
   override def evaluate(buffer: Row): Any = {
     buffer.getAs[Array[Long]](0)
   }
